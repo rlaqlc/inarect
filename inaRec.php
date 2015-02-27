2f0316a451f85d55b5255d2ce8f2a24d6eda3784
ojw0618 <semperfi.jay@gmail.com>
2015-02-26 19:53:46 -0700

    The first draft for log-in page.

--- inaRec.php
+++ inaRec.php
@@ -1,4 +1,14 @@
-<?php
-echo 'hello world'
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>User Log-In </title>
+    </head>
+    <body>
+        User ID: <input type ="text" id = "ID" size = "4">
+        Password: <input type ="text" id = "password" size = "4">
+        <button type="button" onclick="login()">LogIn</button>
+
+    </body>
+</html>
 
